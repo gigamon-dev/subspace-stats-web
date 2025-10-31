c372ce37023a52d950dae6b94a505802ad41cab2
gigamon <gigamon@hotmail.com>
2025-10-31 12:24:07 -0400

    Adjustments for PR #1
    Options:
    - Move options to the SubspaceStats.Options namespace.
    - Move authentication email options to its own section in appsettings.json.
    - FromEmail and FromName to not be nullable.
    
    EmailSender service:
    - Send body as both plain text and html.
    - Logging to use templates instead of interpolated strings.
    - Logging calls to call LogError on errors.
    - Only include the email service when not in the development environment. To test it in development, comment out the check from Program.cs.
    
    Switch the default log level for Microsoft.AspNetCore back to "Warning".

--- src/SubspaceStats/Services/EmailSender.cs
+++ src/SubspaceStats/Services/EmailSender.cs
@@ -1,104 +1,144 @@
-﻿using SubspaceStats.Areas.Identity.Data;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using SubspaceStats.Areas.Identity.Data;
+using SubspaceStats.Options;
 using System.Net.Mail;
+using System.Text;
 
 namespace SubspaceStats.Services;
 
-public class EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+public class EmailSender(
+    IOptions<AuthenticationEmailOptions> optionsAccessor,
     ILogger<EmailSender> logger) : IEmailSender<SubspaceStatsUser>
 {
+    private const string LogEmailSuccessTemplate = "Successfully sent sent email to {emailAddress}.";
+    private const string LogEmailFailureTemplate = "Failed to send email to {emailAddress}. (Response code:{responseCode}, message:{responseMessage})";
+    private const string LogEmailNotConfigured = "Failed to send email because email options are not configured.";
+
+    private readonly AuthenticationEmailOptions _options = optionsAccessor.Value;
     private readonly ILogger _logger = logger;
-    
-    public AuthMessageSenderOptions Options { get; } = optionsAccessor.Value;   
 
-    public Task SendConfirmationLinkAsync(SubspaceStatsUser user, string email,
-       string confirmationLink) => SendEmailAsync(email, "Confirm your email",
-       "<html lang=\"en\"><head></head><body>Please confirm your account by " +
-       $"<a href='{confirmationLink}'>clicking here</a>.</body></html>");
-
-    public Task SendPasswordResetLinkAsync(SubspaceStatsUser user, string email,
-        string resetLink) => SendEmailAsync(email, "Reset your password",
-        "<html lang=\"en\"><head></head><body>Please reset your password by " +
-        $"<a href='{resetLink}'>clicking here</a>.</body></html>");
-
-    public Task SendPasswordResetCodeAsync(SubspaceStatsUser user, string email,
-        string resetCode) => SendEmailAsync(email, "Reset your password",
-        "<html lang=\"en\"><head></head><body>Please reset your password " +
-        $"using the following code:<br>{resetCode}</body></html>");
-
-
-    public async Task SendEmailAsync(string toEmail, string subject, string message)
+    public Task SendConfirmationLinkAsync(SubspaceStatsUser user, string email, string confirmationLink)
     {
-        if (Options.SendGridOverSmtp)
+        return SendEmailAsync(
+            email,
+            "Confirm your email",
+            $"""Please confirm your account by going to {confirmationLink}""",
+            $"""<html lang="en"><head></head><body>Please confirm your account by <a href="{confirmationLink}">clicking here</a>.</body></html>""");
+    }
+
+    public Task SendPasswordResetLinkAsync(SubspaceStatsUser user, string email, string resetLink)
+    {
+        return SendEmailAsync(
+            email, 
+            "Reset your password",
+            $"""Please reset your password by going to {resetLink}""",
+            $"""<html lang="en"><head></head><body>Please reset your password by <a href="{resetLink}">clicking here</a>.</body></html>""");
+    }
+
+    public Task SendPasswordResetCodeAsync(SubspaceStatsUser user, string email, string resetCode)
+    {
+        return SendEmailAsync(
+            email, 
+            "Reset your password",
+            $"""Please reset your password using the following code: {resetCode}""",
+            $"""<html lang="en"><head></head><body>Please reset your password using the following code:<br>{resetCode}</body></html>""");
+    }
+
+    private async Task SendEmailAsync(string toEmail, string subject, string plainMessage, string htmlMessage)
+    {
+        if (!string.IsNullOrEmpty(_options.SendGridKey))
         {
-            if (string.IsNullOrEmpty(Options.SendGridKey))
-            {
-                throw new Exception("Null SendGridKey");
-            }
-            await Execute(Options.SendGridKey, subject, message, toEmail);
-        } 
+            await ExecuteSendGrid(_options.SendGridKey, toEmail, subject, plainMessage, htmlMessage);
+        }
+        else if (!string.IsNullOrEmpty(_options.SmtpHost) && !string.IsNullOrEmpty(_options.SmtpUsername) && !string.IsNullOrEmpty(_options.SmtpPassword))
+        {
+            await ExecuteSmtp(toEmail, subject, plainMessage, htmlMessage);
+        }
         else
         {
-            if (string.IsNullOrEmpty(Options.SmtpHost) || string.IsNullOrEmpty(Options.SmtpUsername) || string.IsNullOrEmpty(Options.SmtpPassword))
-            {
-                throw new Exception("Missing SMTP Config");
-            }
-            await ExecuteSmtp(toEmail, subject, message);
+            throw new Exception(LogEmailNotConfigured);
         }
     }
 
-    public async Task Execute(string apiKey, string subject, string message, string toEmail)
+    private async Task ExecuteSendGrid(string apiKey, string toEmail, string subject, string plainMessage, string htmlMessage)
     {
         var client = new SendGridClient(apiKey);
         var msg = new SendGridMessage()
         {
-            From = new EmailAddress(Options.SmtpFromEmail!, Options.SmtpFromName!),
+            From = new EmailAddress(_options.FromEmail!, _options.FromName!),
             Subject = subject,
-            PlainTextContent = message,
-            HtmlContent = message
+            PlainTextContent = plainMessage,
+            HtmlContent = htmlMessage,
         };
         msg.AddTo(new EmailAddress(toEmail));
 
         // Disable click tracking.
         // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
         msg.SetClickTracking(false, false);
+
+        // Send the email.
         var response = await client.SendEmailAsync(msg);
-        _logger.LogInformation(response.IsSuccessStatusCode
-                               ? $"Email to {toEmail} queued successfully!"
-                               : $"Failure Email to {toEmail}");
+
+        // Check the repsonse.
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation(LogEmailSuccessTemplate, toEmail);
+        }
+        else
+        {
+            _logger.LogError(
+                LogEmailFailureTemplate, 
+                toEmail, 
+                response.StatusCode, 
+                await response.Body.ReadAsStringAsync());
+        }
     }
 
-    public async Task ExecuteSmtp(string email, string subject, string htmlMessage)
+    private async Task ExecuteSmtp(string toEmail, string subject, string plainMessage, string htmlMessage)
     {
+        // TODO: Replace use of System.Net.Mail with MailKit.
         SmtpClient smtpClient = new();
-        smtpClient.UseDefaultCredentials = Options.SmtpUseDefaultCredentials;
-        smtpClient.EnableSsl = Options.SmtpSSL;
-        smtpClient.Host = Options.SmtpHost!;
-        smtpClient.Port = Options.SmtpPort;
-        smtpClient.Credentials = new System.Net.NetworkCredential(Options.SmtpUsername, Options.SmtpPassword);
+        smtpClient.UseDefaultCredentials = _options.SmtpUseDefaultCredentials;
+        smtpClient.EnableSsl = _options.SmtpSSL;
+        smtpClient.Host = _options.SmtpHost!;
+        smtpClient.Port = _options.SmtpPort;
+        smtpClient.Credentials = new System.Net.NetworkCredential(_options.SmtpUsername, _options.SmtpPassword);
 
-        var mailMessage = new MailMessage
+        MailMessage mailMessage = new()
         {
-            From = new MailAddress(Options.SmtpFromEmail!, Options.SmtpFromName!),
+            From = new MailAddress(_options.FromEmail, _options.FromName),
             Subject = subject,
-            Body = htmlMessage,
-            IsBodyHtml = true,
         };
-        mailMessage.To.Add(email);
+        mailMessage.To.Add(toEmail);
+
+        // Set the body.
+        mailMessage.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(plainMessage, Encoding.UTF8, "text/plain"));
+        mailMessage.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(htmlMessage, Encoding.UTF8, "text/html"));
 
         try
         {
             await smtpClient.SendMailAsync(mailMessage);
-            Console.WriteLine($"Successful Email to {email}");
-            _logger.LogInformation($"Successful Email to {email}");
+
+            _logger.LogInformation(LogEmailSuccessTemplate, toEmail);
+        }
+        catch (SmtpException smtpEx)
+        {
+            _logger.LogError(
+                LogEmailFailureTemplate,
+                toEmail,
+                smtpEx.StatusCode,
+                smtpEx.Message);
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Failure Email to {email}", ex.Message.ToString());
-            _logger.LogInformation($"Failure Email to {email}", ex.Message.ToString());
+            _logger.LogError(
+                LogEmailFailureTemplate,
+                toEmail,
+                "(none)",
+                ex.Message);
         }
     }
 }
